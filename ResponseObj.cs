eda3975e7434a74b360b4ceb1d34e16aff78adbf
APIVerve Team <hello@apiverve.com>
2024-07-17 12:36:19 -0500

    Bug Fixes

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -6,26 +6,17 @@ namespace APIVerve
 {
 public class data
 {
-    [JsonProperty("domainStatus")]
-    public string[] domainStatus { get; set; }
+    [JsonProperty("createdDate")]
+    public DateTime createdDate { get; set; }
+
+    [JsonProperty("dNSSEC")]
+    public string dNSSEC { get; set; }
 
     [JsonProperty("domainName")]
     public string domainName { get; set; }
 
-    [JsonProperty("registryDomainID")]
-    public string registryDomainID { get; set; }
-
-    [JsonProperty("registrarWHOISServer")]
-    public string registrarWHOISServer { get; set; }
-
-    [JsonProperty("registrarURL")]
-    public string registrarURL { get; set; }
-
-    [JsonProperty("updatedDate")]
-    public DateTime updatedDate { get; set; }
-
-    [JsonProperty("createdDate")]
-    public DateTime createdDate { get; set; }
+    [JsonProperty("domainStatus")]
+    public string[] domainStatus { get; set; }
 
     [JsonProperty("expiryDate")]
     public DateTime expiryDate { get; set; }
@@ -33,17 +24,26 @@ public class data
     [JsonProperty("registrar")]
     public string registrar { get; set; }
 
-    [JsonProperty("registrarIANAID")]
-    public string registrarIANAID { get; set; }
-
     [JsonProperty("registrarAbuseContactEmail")]
     public string registrarAbuseContactEmail { get; set; }
 
     [JsonProperty("registrarAbuseContactPhone")]
     public string registrarAbuseContactPhone { get; set; }
 
-    [JsonProperty("dNSSEC")]
-    public string dNSSEC { get; set; }
+    [JsonProperty("registrarIANAID")]
+    public string registrarIANAID { get; set; }
+
+    [JsonProperty("registrarURL")]
+    public string registrarURL { get; set; }
+
+    [JsonProperty("registrarWHOISServer")]
+    public string registrarWHOISServer { get; set; }
+
+    [JsonProperty("registryDomainID")]
+    public string registryDomainID { get; set; }
+
+    [JsonProperty("updatedDate")]
+    public DateTime updatedDate { get; set; }
 
 }
 

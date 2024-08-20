3af710465f3f617c6e7f6f90a44a4cab3839b8b6
APIVerve Team <hello@apiverve.com>
2024-08-20 13:35:40 -0500

    Bug Fixes

--- WHOISLookupAPIClient.cs
+++ WHOISLookupAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, whoislookupQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, WHOISLookupQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(whoislookupQueryOptions options = null)
+        public ResponseObj Execute(WHOISLookupQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(whoislookupQueryOptions options)
+        private string constructURL(WHOISLookupQueryOptions options)
         {
             string url = _apiEndpoint;
 
